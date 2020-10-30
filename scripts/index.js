956cb410d9867ae57128c1103cb17b59aa5b8cef
Arkady Flitman <arflit@gmail.com>
2020-10-30 09:06:07 +0300

    feat: блок popup и логика на javascript свёрстаны

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close')
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form')
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#job');
+
+function openPopup () {
+popup.classList.add('popup_opened');
+nameInput.setAttribute('value', profileName.textContent);
+jobInput.setAttribute('value', profileJob.textContent);
+}
+
+function closePopup () {
+popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+evt.preventDefault();
+let newName = document.querySelector('#name');
+let newJob = document.querySelector('#job');
+console.log(newName.value);
+profileName.textContent = newName.value;
+profileJob.textContent = newJob.value;
+closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
