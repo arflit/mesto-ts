ed8f6390261863d02dd2d1858cf0d5283f9e9210
Arkady Flitman <arflit@gmail.com>
2020-11-20 03:56:17 +0300

    fix: переименование блока попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 let editButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close')
-let popup = document.querySelector('.popup');
+let popupNameCloseButton = document.querySelector('.popup-name__close')
+let popupName = document.querySelector('.popup-name');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let formElement = document.querySelector('.popup__form')
+let formElement = document.querySelector('.popup-name__form')
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#job');
 
-function openPopup () {
-popup.classList.add('popup_opened');
+function openPopupName () {
+popupName.classList.add('popup-name_opened');
 nameInput.setAttribute('value', profileName.textContent);
 jobInput.setAttribute('value', profileJob.textContent);
 }
 
-function closePopup () {
-popup.classList.remove('popup_opened');
+function closePopupName () {
+popupName.classList.remove('popup-name_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -24,13 +24,13 @@ let newJob = document.querySelector('#job');
 console.log(newName.value);
 profileName.textContent = newName.value;
 profileJob.textContent = newJob.value;
-closePopup();
+closePopupName();
 }
 
-editButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopupName);
+popupNameCloseButton.addEventListener('click', closePopupName);
 formElement.addEventListener('submit', formSubmitHandler);
 document.addEventListener('keydown', function(e) {
   let keyCode = e.keyCode;
-  if (keyCode === 27) closePopup();
+  if (keyCode === 27) closePopupName();
 });
