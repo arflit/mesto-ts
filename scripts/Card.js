bd9cdfd0b7fe139157ef75700a362617a74b92db
Arkady Flitman <arflit@gmail.com>
2020-12-07 02:12:33 +0300

    refactor: функция создания каротчек вынесена в отдельный класс

---
+++ scripts/Card.js
@@ -0,0 +1,68 @@
+import { showPopup } from './index.js';
+
+export class Card {
+
+  constructor (placeTitle, placeImage, cardSelector) {
+    this._title = placeTitle;
+    this._image = placeImage;
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+    .querySelector(this._cardSelector)
+    .content
+    .querySelector('.card')
+    .cloneNode(true);
+
+    return cardElement;
+  }
+
+  _handleLikeClick() {
+    this._element.querySelector('.card__like').classList.toggle('card__like_active');
+  }
+
+  _handleCardRemove() {
+    this._element.remove();
+  }
+
+  _openPopupBigPicture () {
+    const popupBigPicture = document.querySelector('#popup-bigpicture');
+    const popupPicture = popupBigPicture.querySelector('.popup__picture');
+    const popupTitle = popupBigPicture.querySelector('.popup__pic-title');
+    
+    popupPicture.src = this._image;
+    popupPicture.alt = this._title;
+    popupTitle.textContent = this._title;
+
+    showPopup(popupBigPicture);
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.card__like').addEventListener('click', () => {
+      this._handleLikeClick();
+    });
+    this._element.querySelector('.card__trash-button').addEventListener('click', () => {
+      this._handleCardRemove();
+    });
+    this._element.querySelector('.card__image').addEventListener('click', () => {
+      this._openPopupBigPicture();
+    });
+  }
+
+
+  generateCard() {
+    this._element = this._getTemplate();
+
+    const _picture = this._element.querySelector('.card__image');
+    _picture.src = this._image;
+    _picture.alt = this._title;
+    this._element.querySelector('.card__title').textContent = this._title;
+    
+    this._setEventListeners();
+
+    return this._element;
+
+  }
+};
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 //Попапы
 const popupProfile = document.querySelector('#popup-profile');
 const popupAddCard = document.querySelector('#popup-addcard');
-const popupBigPicture = document.querySelector('#popup-bigpicture');
 //Кнопки вызова
 const editButton = document.querySelector('.profile__edit-button');
 const newPlaceButton = document.querySelector('.profile__add-button');
@@ -15,15 +14,12 @@ const profileNameField = profileForm.elements.name;
 const profileJobField = profileForm.elements.job;
 const newCardPlaceField = newCardForm.elements.place;
 const newCardPictureField = newCardForm.elements.picture;
-//Попап с большой картинкой
-const popupBigPicturePicture = popupBigPicture.querySelector('.popup__picture');
-const popupBigPictureTitle = popupBigPicture.querySelector('.popup__pic-title');
 //Содержимое страницы 
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 //Добавление карточек
 const cardsList = document.querySelector('.cards__list');
-const tempCard = document.querySelector('#tempcard').content;
+import { Card } from './Card.js';
 
 //закрытие попапов
 
@@ -58,7 +54,7 @@ popupHideButtons.forEach(function (button) {
 
 //открытие попапов
 
-function showPopup(popup) {
+export function showPopup(popup) {
   popup.classList.add('popup_opened');
   addOverlayListener(popup);
   addEscapeListerner(popup);
@@ -69,45 +65,9 @@ function openPopupProfile () {
   showPopup(popupProfile);
 }
 
-function openPopupBigPicture (image, title) {
-  popupBigPicturePicture.src = image;
-  popupBigPicturePicture.alt = title;
-  popupBigPictureTitle.textContent = title;
-  showPopup(popupBigPicture);
-}
-
-//добавление карточки
-function getCard (placeTitle, placeImage) {
-  const cardElement = tempCard.cloneNode(true);
-  const picture = cardElement.querySelector('.card__image');
-  const title = cardElement.querySelector('.card__title');
-  picture.src = placeImage;
-  picture.alt = placeTitle;
-  title.textContent = placeTitle;
-
-  const likeButton = cardElement.querySelector('.card__like');
-  likeButton.addEventListener('click', function (evt) {
-    const eventTarget = evt.target;
-    eventTarget.classList.toggle('card__like_active');
-  });
-
-  picture.addEventListener('click', function () {
-    openPopupBigPicture(placeImage, placeTitle);
-  });
-
-  const trashButton = cardElement.querySelector('.card__trash-button');
-  trashButton.addEventListener('click', function (evt) {
-    const delEventTarget = evt.target;
-    delEventTarget.closest('.card').remove();
-  }); 
-
-  return cardElement;
-}
-
-
 function addCard (placeTitle, placeImage) {
-  const cardElement = getCard (placeTitle, placeImage);
-  cardsList.prepend(cardElement);
+  const cardElement = new Card(placeTitle, placeImage, '.tempcard');
+  cardsList.prepend(cardElement.generateCard());
 }
 
 //добавление карточек на старте
